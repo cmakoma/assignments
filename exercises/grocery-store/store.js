4c66667388b51d4909bd5bb68083cafb2fa041f2
christian makoma <nkndchris@gmail.com>
2019-01-25 15:20:47 -0700

    first commit

---
+++ exercises/grocery-store/store.js
@@ -0,0 +1,11 @@
+var shopper = {
+    fruits : ['orange', 'avocado', 'banana'],
+    visaCard : true,
+    totalPound : 100,
+    checkOut : function(rating){
+        console.log("your ratind is "+ rating)
+    }
+
+}
+shopper.checkOut(9)
+// console.log(shopper.fruits[2])
---
+++ exercises/hello-class/app.js
@@ -0,0 +1,5 @@
+var printName = function (name,age){
+    console.log("hello am "+ name + " , I have been in utah for "+age +" years")
+}
+printName("christian",2);
+printName("montego",1);
