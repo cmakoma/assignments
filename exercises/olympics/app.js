cc5c7de6f580b6237da1b08b31ff5822f3e9b356
christian makoma <nkndchris@gmail.com>
2019-01-28 13:20:36 -0700

    if olympics

---
+++ exercises/olympics/app.js
@@ -0,0 +1,121 @@
+// num = 5;
+// if(num>3){
+//     console.log("num" + " is greater than 3");
+// }
+
+
+
+
+// if("cat".length === 3){
+//     console.log("is the length");
+// }
+
+
+// if("cat" === "dog"){
+//     console.log("same");
+// }
+// else{
+//         console.log("not the same")
+// }
+
+
+// var person = {
+//     name: "Bobby",
+//     age: 12
+// }
+
+// if(person.age >= 18){
+//     console.log(person.name  + " is allow to go to the movie")
+// }
+// else{
+//     console.log(person.name  + " is not allow to go to the movie")
+// }
+
+
+// var person = {
+//     name: "Bobby",
+//     age: 12
+// }
+
+// if(person.name.startsWith("B")){
+//     console.log(person.name  + " is allow to go to the movie")
+// }
+// else{
+//     console.log(person.name  + " is not allow to go to the movie")
+// }
+
+
+// var person = {
+//     name: "Bobby",
+//     age: 12
+// }
+
+// if(person.name.startsWith("B") && person.age >= 18){
+//     console.log(person.name  + " is allow to go to the movie")
+// }
+// else{
+//     console.log(person.name  + " is not allow to go to the movie")
+// }
+
+
+
+// num = 1
+
+// if(num === "1"){
+//     console.log("srtict")
+// } 
+//     else if(num == "1"){
+//     console.log("abstract");
+//     }
+    
+//         else{
+//     console.log("not equal at all")
+//         }
+
+
+// num = 2
+
+// if(1 <= num && num*num == 4){
+//     console.log("yes")
+// }
+// else {
+//     console.log("boooo")
+// }
+
+
+
+
+// if(typeof("dog") === 'string'){
+//     console.log("this is a string")
+// }
+
+
+
+
+// if(typeof(true) === 'boolean'){
+//     console.log("this is a boolean")
+// }
+
+// num = 45
+
+// if(typeof(num) !== 'undefined'){
+//     console.log("waaaoh")
+// }
+// else{
+//     console.log(" booo")
+// }
+
+// num = 12
+
+// if ( "s" >= num){
+//     console.log("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+// num = 8
+// num%2 === 0 ? console.log("even") : console.log("odd")
+
+
+                        done
