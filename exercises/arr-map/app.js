0f2c2d2d527f715e4e9aa9c61c3983a41f84961a
christian makoma <nkndchris@gmail.com>
2019-02-07 15:30:43 -0700

    constructive functon

---
+++ exercises/arr-map/app.js
@@ -0,0 +1,66 @@
+// numbers = [2,5,100]
+
+// doubleNumbers = numbers.map(function(number){
+//     return number*2
+// })
+// console.log(numbers)
+// console.log(doubleNumbers)
+
+// numbers = [2,5,100]
+// strNumbers = numbers.map(function(number){
+//     return number.toString()
+// })
+// console.log(strNumbers)
+
+// names=["chris","CHRISTIAN","makoma"]
+
+// captNames = names.map(function(name){
+//     return name.toUpperCase()[0]+name.slice(1).toLowerCase()
+// })
+// console.log(captNames)
+
+
+persons=[
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+]
+
+
+// names = persons.map(function(person){
+//     return person.name
+// })
+
+// console.log(names)
+
+// matrix = persons.map(function(person){
+// if(person.age>18){
+//     return person.name + " can go to matrix"
+// }else{
+//     return person.name + " is under age!!!!"
+// }
+// })
+
+// console.log(matrix)
+
+dom = persons.map(function(person){
+    return "<h1>" +person.name +"</h1><h2>"+person.age+"</h2>"
+})
+console.log(dom)
